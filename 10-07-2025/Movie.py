2d709a017260a47c1e9a05ab84f3e8d035281bb9
LIKHITHAKATLA <likitha.katla@hexanovamedia.tech>
2025-07-11 20:06:59 +0530

    11-07-2025
    completed Moive project

--- 10-07-2025/Movie.py
+++ 10-07-2025/Movie.py
@@ -1,4 +1,6 @@
 import requests
+import os
+import json
 def Movie(title):
     api_key = "641f48d5"
     url = f"http://www.omdbapi.com/?apikey={api_key}&t={title}"
@@ -6,13 +8,34 @@ def Movie(title):
     if movie_data_json.status_code == 200:
             movie_data = movie_data_json.json()
             if movie_data.get("Response") == "True":
-                print(movie_data)
-                print("Title: ",movie_data["Title"])
-                print("Year: ",movie_data["Year"])
-                print("Reeased Date: ",movie_data["Released"])
-                print("Director: ",movie_data["Director"])
-                print("Writer: ",movie_data["Writer"])
-                print("Language: ",movie_data["Language"])
+                data = {
+                      "Title" : movie_data["Title"],
+                      "Year" : movie_data["Year"],
+                      "Released Date": movie_data["Released"],
+                      "Director": movie_data["Director"],
+                      "Writer": movie_data["Writer"],
+                      "Language" : movie_data["Language"]
+                      }
+                # print(data)
+                if os.path.exists("Movie.json"):
+                    with open("Movie.json","r") as f:
+                        try:
+                            existsdata = json.load(f)
+                        except Exception as e:
+                             existsdata = []
+                else:
+                     existsdata = []
+                existsdata.append(data)
+                with open("Movie.json", "w") as f:
+                    json.dump(existsdata, f, indent=4)
+                        
+                # print(movie_data)
+                # print("Title: ",movie_data["Title"])
+                # print("Year: ",movie_data["Year"])
+                # print("Reeased Date: ",movie_data["Released"])
+                # print("Director: ",movie_data["Director"])
+                # print("Writer: ",movie_data["Writer"])
+                # print("Language: ",movie_data["Language"])
             else:
                 print("Movie not found!")
     else:
