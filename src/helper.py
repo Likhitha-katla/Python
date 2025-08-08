e2de3155ef67756d17ec60ba56220c56a2993d86
Likhitha-katla <likitha.katla@hexanovamedia.tech>
2025-08-08 20:09:17 +0530

    Add files via upload

---
+++ src/helper.py
@@ -0,0 +1,22 @@
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+def load_pdf_file(data):
+    loader = DirectoryLoader(data,
+                             glob="*.pdf",
+                             loader_cls=PyPDFLoader)
+    documents = loader.load()
+    return documents
+
+def text_spilt(extracted_data):
+    text_spiltter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+    text_chunks = text_spiltter.split_documents(extracted_data)
+    return text_chunks
+
+def download_hugging_face_embeddings():
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    return embeddings
+# print("success")
